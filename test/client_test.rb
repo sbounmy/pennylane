61256d405c84f587fe251dad980d69c3d2612005
Stephane Bounmy <159814+sbounmy@users.noreply.github.com>
2024-05-19 15:03:03 +0200

    vcr client test

--- test/client_test.rb
+++ test/client_test.rb
@@ -8,14 +8,14 @@ class Pennylane::ClientTest < Test::Unit::TestCase
 
   class RequestTest < Pennylane::ClientTest
     test "works on get" do
-      resp = client.request(:get, '/customers', params: { body: {} })
+      resp = vcr { client.request(:get, '/customers', params: { body: {} }) }
 
       obj = JSON.parse(resp.read_body)
       assert obj['total_customers'] > 19
     end
 
     test "works on get with params" do
-      resp = client.request(:get, '/customers', params: { body: { page: 2 } })
+      resp = vcr { client.request(:get, '/customers', params: { body: { page: 2 } }) }
 
       obj = JSON.parse(resp.read_body)
       assert_equal 2, obj['current_page']
@@ -23,8 +23,7 @@ class Pennylane::ClientTest < Test::Unit::TestCase
 
     test "works on post" do
       customer_params = { customer_type: 'company', name: 'Tesla', address: '4 rue du faubourg', postal_code: '75008', city: 'Paris', country_alpha2: 'FR' }
-      resp = client.request(:post, '/customers', params: { body: { customer: customer_params } }
-                              )
+      resp = vcr { client.request(:post, '/customers', params: { body: { customer: customer_params } }) }
 
       obj = JSON.parse(resp.read_body)
       assert_equal 'Tesla', obj['customer']['name']
@@ -36,19 +35,19 @@ class Pennylane::ClientTest < Test::Unit::TestCase
 
     test 'raise AuthenticationError when wrong key' do
       assert_raises Pennylane::AuthenticationError do
-        client('blabla').request(:get, '/customers')
+        vcr { client('blabla').request(:get, '/customers') }
       end
     end
 
     test 'raise Not found error when missing resource' do
       assert_raises Pennylane::NotFoundError do
-        client.request(:get, '/customers/123')
+        vcr { client.request(:get, '/customers/123') }
       end
     end
 
     test 'raise error when wrong url' do
       assert_raises Pennylane::NotFoundError do
-        client.request(:get, '/something')
+        vcr { client.request(:get, '/something') }
       end
     end
   end
